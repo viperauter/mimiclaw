de2fc47897f8c1fffced8261ea45865221c6be47
root <root@matebook.localdomain>
2026-03-15 11:44:28 +0800

    modif cmake temp

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -4,17 +4,33 @@ add_compile_options(
     -Wno-unused-parameter
 )
 
-if(WIN32)
+# TLS backend configuration
+if(TLS_BACKEND STREQUAL "builtin")
     add_compile_definitions(
         MG_TLS=MG_TLS_BUILTIN
+    )
+elseif(TLS_BACKEND STREQUAL "openssl")
+    add_compile_definitions(
+        MG_TLS=MG_TLS_OPENSSL
+    )
+else()
+    # Default fallback
+    if(WIN32)
+        add_compile_definitions(
+            MG_TLS=MG_TLS_BUILTIN
+        )
+    else()
+        add_compile_definitions(
+            MG_TLS=MG_TLS_OPENSSL
+        )
+    endif()
+endif()
+
+if(WIN32)
+    add_compile_definitions(
         _WIN32_WINNT=0x0601
     )
     add_compile_options(
         -D_POSIX_C_SOURCE=200809L
-    )
-else()
-    # On non-Windows (POSIX) use OpenSSL-backed TLS for Mongoose
-    add_compile_definitions(
-        MG_TLS=MG_TLS_OPENSSL
     )
 endif()
